64f8daafa67dab960888cd274d8d07fe80938bf5
liuxiaofei99 <94382707+liuxiaofei99@users.noreply.github.com>
2021-11-23 16:58:59 +0800

    Add files via upload

---
+++ ui/src/App.jsx
@@ -0,0 +1,10 @@
+import 'babel-polyfill';
+import 'whatwg-fetch';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import IssueList from './IssueList.jsx';
+
+const element = <IssueList />;
+
+ReactDOM.render(element, document.getElementById('contents'));
